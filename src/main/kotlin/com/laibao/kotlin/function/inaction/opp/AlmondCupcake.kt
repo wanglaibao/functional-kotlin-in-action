ad1cb426aaa556bfbea1816dba98e02a1812ad82
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-13 18:59:18 +0800

    update pom.xml file info

---
+++ src/main/kotlin/com/laibao/kotlin/function/inaction/opp/AlmondCupcake.kt
@@ -0,0 +1,8 @@
+package com.laibao.kotlin.function.inaction.opp
+
+/**
+ * @author laibao wang
+ */
+class AlmondCupcake {
+    val flavour = "Almond"
+}
---
+++ src/main/kotlin/com/laibao/kotlin/function/inaction/opp/Cupcake.kt
@@ -0,0 +1,8 @@
+package com.laibao.kotlin.function.inaction.opp
+
+/**
+ * @author laibao wang
+ */
+class Cupcake (val flavour: String){
+    //val flavour = flavour
+}
