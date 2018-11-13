6e14fb7dff42ba98323cba6e935868ac75d9efb7
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-13 17:34:14 +0800

    update pom.xml file info

---
+++ src/test/kotlin/com/laibao/kotlin/function/inaction/function/inaction/HelloTest.kt
@@ -0,0 +1,12 @@
+package com.laibao.kotlin.function.inaction.function.inaction
+
+import org.junit.Test
+
+class HelloTest {
+
+    @Test
+    fun testHello() {
+        println("hello,let's test kotlin")
+    }
+
+}
