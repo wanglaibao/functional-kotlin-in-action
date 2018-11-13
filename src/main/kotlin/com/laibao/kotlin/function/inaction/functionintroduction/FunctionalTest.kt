cd8392de04e6c661108bac8622d56e11b853e805
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-13 20:40:18 +0800

    update pom.xml file info

--- src/main/kotlin/com/laibao/kotlin/function/inaction/functionintroduction/FunctionalTest.kt
+++ src/main/kotlin/com/laibao/kotlin/function/inaction/functionintroduction/FunctionalTest.kt
@@ -4,4 +4,11 @@ package com.laibao.kotlin.function.inaction.functionintroduction
  * @author laibao wang
  */
 class FunctionalTest {
+
+}
+
+val capitalize = { str: String -> str.capitalize() }
+
+fun main(args: Array<String>) {
+    println(capitalize("hello world!"))
 }
