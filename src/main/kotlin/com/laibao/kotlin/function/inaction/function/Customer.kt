09f8b86f3b58df6e2bc9512b00e79e7a8864b617
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-15 12:36:04 +0800

    update pom.xml file info

---
+++ src/main/kotlin/com/laibao/kotlin/function/inaction/function/Customer.kt
@@ -0,0 +1,14 @@
+package com.laibao.kotlin.function.inaction.function
+
+/**
+ * @author laibao wang
+ */
+typealias Kg = Double
+typealias cm = Int
+
+data class Customer (val firstName: String,
+                     val middleName: String,
+                     val lastName: String,
+                     val passportNumber: String,
+                     val weight: Kg,
+                     val height: cm)
---
+++ src/main/kotlin/com/laibao/kotlin/function/inaction/function/Programmer.kt
@@ -0,0 +1,9 @@
+package com.laibao.kotlin.function.inaction.function
+
+/**
+ * @author laibao wang
+ */
+data class Programmer (val firstName: String,
+                        val lastName: String,
+                        val favouriteLanguage: String = "Java 8",
+                        val yearsOfExperience: Int = 0)
