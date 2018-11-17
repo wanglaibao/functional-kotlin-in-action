d54fcc115d559b4d51c7642ca2137a5de43d3e5e
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-17 15:08:05 +0800

    update pom.xml file info

---
+++ src/main/kotlin/com/laibao/kotlin/function/inaction/collections/MapExample.kt
@@ -0,0 +1,37 @@
+package com.laibao.kotlin.function.inaction.collections
+
+/**
+ * @author laibao wang
+ */
+
+fun main(args: Array<String>) {
+    val map = mapOf<String,Int>(
+            "One".to(1),
+            "Two".to(2),
+            "Three".to(3),
+            "Four".to(4),
+            "Five".to(0),//(1) We placed 0 instead of 5 here, will be replaced later
+            "Six".to(6),
+            "Five".to(5)//(2) This will replace earlier map of "Five".to(0)
+    )
+
+    println("The value at Key `Four` is ${map["Four"]}")
+    println("Contents in map")
+    for(entity in map) {
+        println("Key ${entity.key}, Value ${entity.value}")
+    }
+    println()
+
+    val mutableMap = mutableMapOf<Int,String>()
+    mutableMap.put(1,"Item 1")
+    mutableMap.put(2,"Item 2")
+    mutableMap.put(3,"Item 3")
+    mutableMap.put(4,"Item 4")
+
+    println("Replacing value at key 1 - ${mutableMap.put(1,"Item 5")}")//(3)
+
+    println("Contents in mutableMap")
+    for(entry in mutableMap) {
+        println("Key ${entry.key}, Value ${entry.value}")
+    }
+}
